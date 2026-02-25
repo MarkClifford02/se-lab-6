de3a39493a34997bcf4a42780e5cc1213ff94411
SimplyPrincee <princeguinto3@gmail.com>
2026-02-25 08:51:59 +0800

    Gerald's Clean Frontend

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <h1>AI Mood Tracker</h1>
+    <textarea v-model="mood" placeholder="How are you feeling?"></textarea>
+    <br>
+    <button @click="submitMood">Submit</button>
+    <p v-if="response">{{ response }}</p>
+  </div>
+</template>
+
+<script setup>
+import { ref } from 'vue'
+const mood = ref('')
+const response = ref('')
+
+const submitMood = () => {
+  response.value = "Logic connected! Ready for Backend Part 2."
+}
+</script>
+
+<style scoped>
+div { text-align: center; margin-top: 50px; font-family: sans-serif; }
+textarea { width: 300px; height: 100px; }
+</style>
